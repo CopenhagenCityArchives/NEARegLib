0adb31ca15d36a4f4d4c95634de52ced53151225
Bo Henriksen <stumpdenmark@hotmail.com>
2023-05-22 14:51:52 +0200

    Add NEAReg class and factory

---
+++ NEARegLib.Lib/DAL/UnitOfWork/NEAReg.cs
@@ -0,0 +1,72 @@
+﻿using NEARegLib.Models;
+using NEARegLib.DAL.Repositories;
+using NEARegLib.DAL.UnitsOfWork;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace neaweb_dapper.DAL.UnitOfWork
+{
+    /// <summary>
+    /// This class handles creation of LogEntry objects and subsequent updates of related archiveversionmetadata
+    /// </summary>
+    public class NeaReg
+    {
+        public readonly IArchiveversionMetadataRepository ArchiveversionMetadataRepository;
+        private readonly ISoftwareVersionRepository _softwareVersionRepository;
+        public readonly ILocationRepository LocationRepository;
+        private readonly IGenericRepository<LogEntry> _logEntryRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NeaReg(IUnitOfWork unitOfWork, IArchiveversionMetadataRepository archiveversionMetadataRepository, ISoftwareVersionRepository softwareVersionRepository, ILocationRepository locationRepository, IGenericRepository<LogEntry> logEntryRepository)
+        {
+            _unitOfWork = unitOfWork;
+            ArchiveversionMetadataRepository = archiveversionMetadataRepository;
+            _softwareVersionRepository = softwareVersionRepository;
+            LocationRepository = locationRepository;
+            _logEntryRepository = logEntryRepository;
+        }
+
+        public async Task<bool> UpdateArchiveversionAddLogEntry(ArchiveversionMetadata av, LogEntryType type)
+        {
+            var existingAv = await ArchiveversionMetadataRepository.Retrieve(av.Id) ?? throw new ArgumentException("ArchiveversionMetadata must exist in database when updating it");
+
+            _unitOfWork.StartTransaction();
+
+            try
+            {
+                // Archiveversion already exists - update if it as changed
+                if (!existingAv.Equals(av))
+                {
+                    await ArchiveversionMetadataRepository.Update(av);
+                }
+
+                await AddLogEntry(av.Id, "Updated archiveversion metadata", type);
+            }
+            catch (Exception e)
+            {
+                _unitOfWork.RollBack();
+
+                throw e;
+            }
+
+            _unitOfWork.Commit();
+
+            return true;
+        }
+
+        public async Task<bool> AddLogEntry(int archiveversionId, string description, LogEntryType logEntryType, bool errorsOccurred = false)
+        {
+            var logEntry = new LogEntry()
+            {
+                SoftwareVersionId = _softwareVersionRepository.InsertOrGetSoftwareVersionIdByNameAndVersion("software", "version" + new Random().Next(1000)).Result.Id,
+                Description = description,
+                Type = logEntryType,
+                ArchiveversionId = archiveversionId,
+                ErrorsOccurred = errorsOccurred
+            };
+
+            return await _logEntryRepository.Create(logEntry) != null;
+        }
+    }
+}
---
+++ NEARegLib.Lib/DAL/UnitOfWork/NeaRegFactory.cs
@@ -0,0 +1,36 @@
+﻿using NEARegLib.DAL;
+using NEARegLib.DAL.Repositories;
+using NEARegLib.DAL.UnitsOfWork;
+using System;
+
+namespace neaweb_dapper.DAL.UnitOfWork
+{
+    public class NeaRegFactory
+    {
+        private readonly string _connectionString;
+        public NeaRegFactory(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException("Connection string must be set");
+            }
+
+            _connectionString = connectionString;
+        }
+        public NeaReg GetNew()
+        {
+            // Get connection factory and connection string
+            ConnectionFactory conFac = new ConnectionFactory(_connectionString);
+            var connection = conFac.GetConnection;
+
+            // Get unit of work and repositories
+            var unitOfWork = new NEARegLib.DAL.UnitsOfWork.UnitOfWork(connection);
+            var archiveversionMetadataRepository = new ArchiveversionMetadataRepository(unitOfWork);
+            var logEntryMetadataRepository = new LogEntryRepository(unitOfWork);
+            var softwareVersionRepository = new SoftwareVersionRepository(unitOfWork);
+            var locationRepository = new LocationRepository(unitOfWork);
+
+            return new NeaReg(unitOfWork, archiveversionMetadataRepository, softwareVersionRepository, locationRepository, logEntryMetadataRepository);
+        }
+    }
+}
