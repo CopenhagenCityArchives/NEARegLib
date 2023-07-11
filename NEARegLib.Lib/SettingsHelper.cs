195b438111634d21fdb3426276b4a6dc7777a1a2
Bo Henriksen <stumpdenmark@hotmail.com>
2023-07-11 15:54:28 +0200

    Add SettingsHelper and related resources

---
+++ NEARegLib.Lib/SettingsHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace NEARegLib
+{
+    // Class that loads settings from an external json file
+    public class SettingsLoader
+    {
+        public static Settings Load()
+        {
+            return JsonSerializer.Deserialize<Settings>(File.ReadAllText(Properties.Resources.settingsLocation));
+        }
+    }
+
+    public class Settings
+    {
+        public string DbHost { get; set; }
+        public int DbPort { get; set; }
+        public string DbName { get; set; }
+        public string DbUserName { get; set; }
+        public string DbPassword { get; set; }
+    }
+
+    public class ConnectionStringFactory
+    {
+        public static string GetConnectionString(Settings settings)
+        {
+            return $"Server={settings.DbHost};Database={settings.DbName};Port={settings.DbPort};Uid={settings.DbUserName};Pwd={settings.DbPassword};Convert Zero Datetime=True;DefaultCommandTimeout=3600;Connect Timeout=10;SslMode=None;AllowPublicKeyRetrieval=True;";
+        }
+    }
+}
