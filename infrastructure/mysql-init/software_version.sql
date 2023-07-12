c6b5c8c24b3d7e911eb82727cde7d75cb1a1ed3e
Bo Henriksen <stumpdenmark@hotmail.com>
2023-07-12 13:47:58 +0200

    Update SoftwareVersion to reflect production database

--- infrastructure/mysql-init/software_version.sql
+++ infrastructure/mysql-init/software_version.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `software_version` (
   `id` int NOT NULL AUTO_INCREMENT,
   `program` varchar(50) NOT NULL,
-  `version` varchar(250) NOT NULL,
+  `informationalVersion` varchar(250) NOT NULL,
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `program_version` (`program`,`version`)
+  UNIQUE KEY `program_informationalVersion` (`program`,`informationalVersion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_danish_ci;
